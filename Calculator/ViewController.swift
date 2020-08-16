f32029ff76ebb1f935fba896d741b87af9886efa
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2020-08-16 17:54:57 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Konstantinos Kolioulis on 16/8/20.
+//  Copyright © 2020 Konstantinos Kolioulis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
