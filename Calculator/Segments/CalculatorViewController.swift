3ce6049cb295304581bbf101ea7c57925070e27e
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2020-10-04 15:42:01 +0300

    Initialize numpad in CalculatorVC

--- Calculator/Segments/CalculatorViewController.swift
+++ Calculator/Segments/CalculatorViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var delButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var divButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var multButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var subButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var resultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
