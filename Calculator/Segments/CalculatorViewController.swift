b67f1285640dd45a7b1852d70ecaa38d68b1eaf0
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2020-10-04 15:07:10 +0300

    Initialize buttons for CalculatorVC

--- Calculator/Segments/CalculatorViewController.swift
+++ Calculator/Segments/CalculatorViewController.swift
@@ -12,7 +12,6 @@ class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.red
     }
     
     init() {
@@ -22,5 +21,4 @@ class CalculatorViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
