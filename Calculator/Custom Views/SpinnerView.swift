fb257daf2a97c9b611e15250a590a1ae89548c28
Konstantinos Kolioulis <konstantinoskol@hotmail.com>
2020-10-31 10:36:16 +0200

    Add numpad in CurrencyConverterVC

--- Calculator/Custom Views/SpinnerView.swift
+++ Calculator/Custom Views/SpinnerView.swift
@@ -8,10 +8,47 @@
 
 import UIKit
 
+@IBDesignable
 final class SpinnerView: UIView {
 
+    @IBOutlet var contentView: UIView!
     @IBOutlet private weak var currencyLabel: UILabel!
     @IBOutlet private weak var arrowIcon: UIImageView!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    private func setupView() {
+        setupNib()
+    }
+    
+    private func setupNib() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "SpinnerView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView //swiftlint:disable:this force_cast
+        view.frame = self.bounds
+        addSubview(view)
+        view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.contentView = view
+    }
 
 }
